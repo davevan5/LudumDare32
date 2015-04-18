87daa0cbcbf461326e132e179040c6e6bb98a51e
ginja <davidevans1985@gmail.com>
2015-04-18 14:12:24 +0100

    Move UI Game Bar Class to own file.

--- LudumDare32/LudumDare32.Game/LudumDare32Game.cs
+++ LudumDare32/LudumDare32.Game/LudumDare32Game.cs
@@ -1,13 +1,9 @@
 using System.Threading.Tasks;
-using SiliconStudio.Core;
 using SiliconStudio.Core.Diagnostics;
 using SiliconStudio.Core.Mathematics;
 using SiliconStudio.Paradox;
 using SiliconStudio.Paradox.Effects;
-using SiliconStudio.Paradox.Games.Time;
 using SiliconStudio.Paradox.Graphics;
-using SiliconStudio.Paradox.UI;
-using SiliconStudio.Paradox.UI.Controls;
 using SiliconStudio.Paradox.UI.Panels;
 
 namespace LudumDare32
@@ -28,16 +24,6 @@ namespace LudumDare32
             await base.LoadContent();
 
             CreatePipeline();
-
-            /*staminaBar = new ImageElement {Source = new UIImage(Asset.Load<Texture>("stamina"))};
-            staminaBar.SetCanvasRelativeSize(new Vector3(0.1f));
-            staminaBar.SetCanvasRelativePosition(new Vector3(0.8f, 0.9f, 1f));
-
-            var uiHealth = new UIImage(Asset.Load<Texture>("health"));
-            healthBar = new ImageElement {Source = uiHealth, StretchType = StretchType.Fill};
-            healthBar.Width = healthBar.Source.Texture.Width;
-            healthBar.Height = healthBar.Source.Texture.Height;
-            healthBar.SetCanvasRelativePosition(new Vector3(0.1f, 0.9f, 1f));*/
 
             gameHud = new Canvas();
             var healthBar = new UIGameBar(Services, gameHud);
@@ -64,35 +50,6 @@ namespace LudumDare32
                 // Wait next rendering frame
                 await Script.NextFrame();
             }
-        }
-    }
-
-    public class UIGameBar : ScriptContext
-    {
-        private readonly Canvas gameCanvas;
-
-        private ImageElement barOutline;
-
-        private ImageElement barElement;
-
-        public UIGameBar(IServiceRegistry registry, Canvas gameCanvas)
-            : base(registry)
-        {
-            this.gameCanvas = gameCanvas;
-        }
-
-        public void LoadContent()
-        {
-            var barOutlineTexture = Asset.Load<Texture>("bar_outline");
-            barOutline = new ImageElement
-            {
-                Source = new UIImage(barOutlineTexture),
-                Width = barOutlineTexture.Width,
-                Height = barOutlineTexture.Height
-            };
-            barOutline.SetCanvasRelativePosition(new Vector3(0.1f, 0.5f, 1f));
-
-            gameCanvas.Children.Add(barOutline);
         }
     }
 }
---
+++ LudumDare32/LudumDare32.Game/UIGameBar.cs
@@ -0,0 +1,39 @@
+using SiliconStudio.Core;
+using SiliconStudio.Core.Mathematics;
+using SiliconStudio.Paradox;
+using SiliconStudio.Paradox.Graphics;
+using SiliconStudio.Paradox.UI;
+using SiliconStudio.Paradox.UI.Controls;
+using SiliconStudio.Paradox.UI.Panels;
+
+namespace LudumDare32
+{
+    public class UIGameBar : ScriptContext
+    {
+        private readonly Canvas gameCanvas;
+
+        private ImageElement barOutline;
+
+        private ImageElement barElement;
+
+        public UIGameBar(IServiceRegistry registry, Canvas gameCanvas)
+            : base(registry)
+        {
+            this.gameCanvas = gameCanvas;
+        }
+
+        public void LoadContent()
+        {
+            var barOutlineTexture = Asset.Load<Texture>("bar_outline");
+            barOutline = new ImageElement
+            {
+                Source = new UIImage(barOutlineTexture),
+                Width = barOutlineTexture.Width,
+                Height = barOutlineTexture.Height
+            };
+            barOutline.SetCanvasRelativePosition(new Vector3(0.1f, 0.5f, 1f));
+
+            gameCanvas.Children.Add(barOutline);
+        }
+    }
+}
